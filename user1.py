ee95bd68d6bc3ccd3d92e690a65251a66c05a88d
Tanner Larson <tannerlarson@Tanners-MacBook-Pro.local>
2016-10-17 06:59:12 -0700

    user1 solution

---
+++ user1.py
@@ -0,0 +1,11 @@
+import hashlib
+import random
+
+user_1 = "1b6453892473a467d07372d45eb05abc2031647a"
+level_1_pass = str(random.randint(0,9))
+user_1_guess_hash = hashlib.sha1(level_1_pass).hexdigest()
+
+while user_1 != user_1_guess_hash:
+    level_1_pass =  str(random.randint(0,9))
+    user_1_guess_hash = hashlib.sha1(level_1_pass).hexdigest()
+print level_1_pass

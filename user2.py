b76c18fbbfc4fd1c2ce277bd554f02b095a5e2da
Tanner Larson <tannerlarson@Tanners-MacBook-Pro.local>
2016-10-18 13:05:44 -0700

    Attempts + Better Print + Efficiency (For Loop)
    user2

--- user2.py
+++ user2.py
@@ -2,16 +2,17 @@ import hashlib
 import random
 
 user_2 = "5c1dc07026ceafcb704bdff1375815f30a769a13"
-level_2_pass = str(random.randint(1,9)) + str(random.randint(0,9)) + str(random.randint(0,9))
-user_2_guess_hash = hashlib.sha1(level_2_pass).hexdigest()
-
-while user_2 != user_2_guess_hash:
-    level_2_pass = str(random.randint(1,9)) + str(random.randint(0,9)) + str(random.randint(0,9))
-    user_2_guess_hash = user_2_guess_hash = hashlib.sha1(level_2_pass).hexdigest()
-print level_2_pass
+count = 0
 
 int = 0
-for a in range(10):
-    int += 1
-
-#ANSWER = 747
+for a in range(1,9):
+    for b in range(10):
+        for c in range(10):
+            level_2_pass = str(a) + str(b) + str(c)
+            user_2_guess_hash = hashlib.sha1(str(level_2_pass)).hexdigest()
+            count += 1
+            if user_2 == user_2_guess_hash:
+                print "Level 2 Password = " + level_2_pass
+                print "Attempts = " + str(count)
+#Level 2 Password = 747
+#Attempts = 648
